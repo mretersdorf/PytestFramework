d68d62bc7407419cc88d0dd2792efd0f35313f55
ElectedShark <17318789+mretersdorf@users.noreply.github.com>
2020-11-05 14:25:49 -0600

    update framework bones

--- tests/sportscar/body/test_body.py
+++
@@ -1,7 +0,0 @@
-from pytest import mark
-
-
-@mark.smoke
-@mark.body
-def test_body_functions_as_exepected():
-    assert True
--- tests/sportscar/entertainment/test_entertainment.py
+++
@@ -1,6 +0,0 @@
-from pytest import mark
-
-
-@mark.entertainment
-def test_entertainment_system_functions_as_expected():
-    assert True
